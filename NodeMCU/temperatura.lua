ba1d67286f965a6f1cd61b34d5e72399a555c17f
Maia-jp <57457490+Maia-jp@users.noreply.github.com>
2019-11-20 22:36:30 -0300

    NodeMCU
    Biblioteca e integração com MQTT

--- NodeMCU/temperatura.lua
+++ NodeMCU/temperatura.lua
@@ -2,12 +2,19 @@ local t = require("ds18b20")
 local pin = 7
 local atual = 0 
 local  loop = true
+local mySsid="UIB"
+local myKey="C@r@i100"
+local minhamat = "1234"
+
+local msgr = require("mqttNodeMCULibrary")
 
 local function readout(temp)
   for addr, temp in pairs(temp) do
     if temp then atual = temp end
   end
 print("Temperatura: "..atual.." °C")
+msgr.sendMessage(atual,minhamat.."love")
 end
 
 t:read_temp(readout, pin, t.C)
+msgr.start("test.mosquitto.org", minhamat, minhamat .. "node", mensagemRecebida)
