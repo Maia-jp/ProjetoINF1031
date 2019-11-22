293c1bea38b58a272bed69289142215686f2bedb
Maia-jp <57457490+Maia-jp@users.noreply.github.com>
2019-11-22 01:29:25 -0300

    Wifi
    Configuração do Wifi para o meu Iphone

---
+++ NodeMCU/Wifi.lua
@@ -0,0 +1,17 @@
+local function wifi_connected_callback(iptable)
+  print("wifi_connected_callback")
+  print("ip: " .. iptable.IP)
+end
+
+wificonf = {
+  ssid = "iPhone de Joao ",
+  pwd = "joao12345",
+  got_ip_cb = wifi_connected_callback,
+  save = true,
+}
+
+wifi.setmode(wifi.STATION)
+wifi.sta.config(wificonf)
+
+  --ssid = "SANACLITA",
+  --pwd = "0335AA3881393675070EF02052AFC87E4AA372B273FFF1242DF13BA85CB69B2D",
